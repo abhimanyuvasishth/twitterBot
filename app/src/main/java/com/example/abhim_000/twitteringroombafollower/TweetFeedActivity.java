23bebb8196cf7afd049edcd471c16c0f135a5625
av1509 <av1509@nyu.edu>
2016-04-17 15:52:17 +0400

    fixes #3, Italian text to speech implemented

--- app/src/main/java/com/example/abhim_000/twitteringroombafollower/TweetFeedActivity.java
+++ app/src/main/java/com/example/abhim_000/twitteringroombafollower/TweetFeedActivity.java
@@ -1,20 +1,32 @@
 package com.example.abhim_000.twitteringroombafollower;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Locale;
+
+// Help for text-speech: http://www.tutorialspoint.com/android/android_text_to_speech.htm
 
 public class TweetFeedActivity extends AppCompatActivity {
     private TweetFeedController controller = new TweetFeedController();
     public static final String TAG = "twitteringRoombaLog";
+    private TextToSpeech tts;
+    private EditText txtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet_feed);
+
+        txtText=(EditText)findViewById(R.id.editText);
+        Button btnSpeak=(Button)findViewById(R.id.button);
 
         final TextView Enter = (TextView) findViewById(R.id.textView);
         Button ClickButton = (Button) findViewById(R.id.radioButton);
@@ -25,5 +37,31 @@ public class TweetFeedActivity extends AppCompatActivity {
                 Enter.setText(controller.getTweets());
             }
         });
+
+        tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.ITALIAN);
+                }
+            }
+        });
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                String toSpeak = txtText.getText().toString();
+                tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH,null,null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(tts !=null){
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
     }
 }
