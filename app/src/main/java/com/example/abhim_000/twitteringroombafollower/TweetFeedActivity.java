d8556f83ee864c680155d511bffd3e732baac08e
av1509 <av1509@nyu.edu>
2016-04-18 20:28:22 +0400

    Language menu bar created

--- app/src/main/java/com/example/abhim_000/twitteringroombafollower/TweetFeedActivity.java
+++ app/src/main/java/com/example/abhim_000/twitteringroombafollower/TweetFeedActivity.java
@@ -7,9 +7,14 @@ import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.Locale;
 
 // Help for text-speech: http://www.tutorialspoint.com/android/android_text_to_speech.htm
@@ -42,8 +47,8 @@ public class TweetFeedActivity extends AppCompatActivity {
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
-                    tts.setLanguage(Locale.ITALIAN);
+                if (status == TextToSpeech.ERROR) {
+                    Toast.makeText(getApplicationContext(),"Text to speech error", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -54,6 +59,33 @@ public class TweetFeedActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String toSpeak = txtText.getText().toString();
                 tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        });
+
+        Spinner dropdown = (Spinner) findViewById(R.id.spinner);
+        String[] items = new String[]{"Italian", "French", "English"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                switch (position){
+                    case 0:
+                        tts.setLanguage(Locale.ITALIAN);
+                        break;
+                    case 1:
+                        tts.setLanguage(Locale.FRENCH);
+                        break;
+                    case 2:
+                        tts.setLanguage(Locale.UK);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                tts.setLanguage(Locale.UK);
             }
         });
     }
